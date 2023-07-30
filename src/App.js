73a69fdffe3340d8f2e71868e8d371a51da90479
MahenderKr <mahendra55555555@gmail.com>
2023-07-30 15:32:09 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [dropdownShow, setDropdownShow] = useState(false);
+  const [dropdownValue, setDropdownValue] = useState("");
+  const dropDownOptions = ["Yes", "Probably Not"];
+
+  //method to handle the visibility of dropdown options
+  const handleDropdownClick = () => {
+    setDropdownShow(!dropdownShow);
+  };
+  const handleSetDropdownValue = (value) => {
+    setDropdownValue(value);
+    setDropdownShow(!dropdownShow);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h3>Should you use a dropdown?</h3>
+      <div className={`dropdown`}>
+        <button onClick={handleDropdownClick} className="dropdown-btn">
+          {dropdownValue === "" ? "Select" : dropdownValue}
+          <span><i className="fa-solid fa-angle-down drop-icon"></i></span>
+        </button>
+        <div
+          className={`dropdown-items ${
+            dropdownShow ? "isVisible" : "isHidden"
+          }`}
         >
-          Learn React
-        </a>
-      </header>
+          {
+            dropDownOptions.map((op, index)=>{
+              return(
+                <div className="dropdown-item" key={index}>
+            <div
+              className="dropdown__link"
+              onClick={() => handleSetDropdownValue(op)}
+            >
+              {op}
+            </div>
+          </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
